5ac4e9bc53353fdb81615294df4c59b163f4630f
Aviu00 <93730715+Aviu00@users.noreply.github.com>
2025-01-05 04:50:06 +0300

    Species and blob fixes (#1262)
    * No non-human combat ghost roles & blob fixes.
    
    * Epic checkfail
    
    * Revert "Epic checkfail"
    
    This reverts commit 740b3b1fdf20411edcf727664d8241b4440239fe.
    
    * Warning
    
    * Thanks rabbit.
    
    * Fix typo.

--- Content.Server/_Goobstation/Blob/BlobTileSystem.cs
+++ Content.Server/_Goobstation/Blob/BlobTileSystem.cs
@@ -4,7 +4,6 @@ using System.Numerics;
 using Content.Server.Construction.Components;
 using Content.Server.Destructible;
 using Content.Server.Emp;
-using Content.Server.Flash;
 using Content.Shared._Goobstation.Blob;
 using Content.Shared._Goobstation.Blob.Components;
 using Content.Shared.Damage;
@@ -35,7 +34,6 @@ public sealed class BlobTileSystem : SharedBlobTileSystem
     [Dependency] private readonly TransformSystem _transform = default!;
     [Dependency] private readonly IRobustRandom _random = default!;
     [Dependency] private readonly NpcFactionSystem _npcFactionSystem = default!;
-    [Dependency] private readonly MobStateSystem _mobState = default!;
 
     private EntityQuery<BlobCoreComponent> _blobCoreQuery;
 
@@ -49,7 +47,6 @@ public sealed class BlobTileSystem : SharedBlobTileSystem
         SubscribeLocalEvent<BlobTileComponent, MapInitEvent>(OnMapInit);
         SubscribeLocalEvent<BlobTileComponent, DestructionEventArgs>(OnDestruction);
         SubscribeLocalEvent<BlobTileComponent, BlobTileGetPulseEvent>(OnPulsed);
-        SubscribeLocalEvent<BlobTileComponent, FlashAttemptEvent>(OnFlashAttempt);
         SubscribeLocalEvent<BlobTileComponent, EntityTerminatingEvent>(OnTerminate);
 
         _blobCoreQuery = GetEntityQuery<BlobCoreComponent>();
@@ -73,17 +70,6 @@ public sealed class BlobTileSystem : SharedBlobTileSystem
             return;
 
         component.Core!.Value.Comp.BlobTiles.Remove(uid);
-    }
-
-    private void OnFlashAttempt(EntityUid uid, BlobTileComponent component, FlashAttemptEvent args)
-    {
-        if (args.Used == null || MetaData(args.Used.Value).EntityPrototype?.ID != "GrenadeFlashBang")
-            return;
-
-        if (component.BlobTileType == BlobTileType.Normal)
-        {
-            _damageableSystem.TryChangeDamage(uid, component.FlashDamage);
-        }
     }
 
     private void OnDestruction(EntityUid uid, BlobTileComponent component, DestructionEventArgs args)
